3baa820ad02c9ba34d4faf697e6ad702b26f914c
RaulBirlem <raul.birlem@gmail.com>
2023-07-09 16:06:57 -0300

    PreviousState

--- my-app-vite-02/src/components/ListRender.jsx
+++ my-app-vite-02/src/components/ListRender.jsx
@@ -4,11 +4,22 @@ const ListRender = () => {
 
     const [list] = useState(["Pedro","Matheus","Andre","Maria"]);
 
-    const [users] = useState([
+    const [users, setUsers] = useState([
         {id:1, name:"Matheus", age:22},
         {id:2, name:"Pedro", age:33},
         {id:3, name:"Andre", age:53}
     ])
+
+    // previous state - usa o valor anterior para transformar 
+    // em um valor novo ex : [users, setUsers]
+    const deleteRandom = () =>{
+        const randomNumber = Math.floor(Math.random() * 4);
+
+        setUsers((prevUsers) => {
+            return prevUsers.filter((user) => randomNumber !== user.id)
+        })
+    }
+
   return (
     <div>
         <ul>
@@ -21,6 +32,7 @@ const ListRender = () => {
                 <li key={user.id}>{user.name} - {user.age}</li>
             ))}
         </ul>
+        <button onClick={deleteRandom}>Delete random user</button>
     </div>
   )
 }
