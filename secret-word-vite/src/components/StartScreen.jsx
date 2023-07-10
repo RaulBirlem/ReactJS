566c04e7a0a721a566dd828a85435d07c6289922
RaulBirlem <raul.birlem@gmail.com>
2023-07-10 17:33:44 -0300

    CSS index.css

---
+++ secret-word-vite/src/components/StartScreen.jsx
@@ -0,0 +1,13 @@
+import './StartScreen.css'
+
+const StartScreen = () => {
+  return (
+    <div className='start'>
+        <h1>Secret Word</h1>
+        <p>Clique abaixo para jogar !</p>
+        <button>Começar</button>
+    </div>
+  )
+}
+
+export default StartScreen
