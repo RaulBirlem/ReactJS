2acadc3105f159942d4884e8c7cb4fe71d3e1579
RaulBirlem <raul.birlem@gmail.com>
2023-07-10 14:27:30 -0300

    CSS Dinâmico inline

--- my-app-vite-css/src/App.jsx
+++ my-app-vite-css/src/App.jsx
@@ -1,11 +1,13 @@
 
 import './App.css'
 import MyComponent from './assets/components/MyComponent'
+import { useState } from 'react';
 
 function App() {
- 
-
+  const n =15;
+  const[name]= useState("Anon");
   return (
+   
     <>
     {/* css global */}
     <h1>React com CSS</h1>
@@ -15,6 +17,9 @@ function App() {
 
     {/* inline css */}
     <p style={{color:"blue", padding:"10px",borderTop:"2px solid red"}}>Elemento estilizado de forma inline </p>
+    {/* CSS Inline dinâmico */}
+    <h2 style={n < 10 ? ({color:"purple"}):({color:"yellow"})}>CSS dinâmico</h2>
+    <h2 style={name=== "Anon" ? ({color:"green",backgroundColor:"#000"}):null}>{name}</h2>
     </>
   )
 }
