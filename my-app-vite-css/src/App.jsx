a056ac7132eab5f4ad61eae695dd725fc459465f
RaulBirlem <raul.birlem@gmail.com>
2023-07-10 13:42:58 -0300

    CSS Component

--- my-app-vite-css/src/App.jsx
+++ my-app-vite-css/src/App.jsx
@@ -1,12 +1,17 @@
 
 import './App.css'
+import MyComponent from './assets/components/MyComponent'
 
 function App() {
  
 
   return (
     <>
+    {/* css global */}
     <h1>React com CSS</h1>
+    {/* css component */}
+    <MyComponent/>
+    <p>App.js paragrafo</p>
     </>
   )
 }
---
+++ my-app-vite-css/src/assets/components/MyComponent.jsx
@@ -0,0 +1,14 @@
+import './MyComponent.css';
+
+const MyComponent = () => {
+  return (
+    <div>
+        
+        <h1>Css Component</h1>
+        <p>Component lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        <p className="my-comp-paragraph">Este css é do componente</p>
+    </div>
+  )
+}
+
+export default MyComponent
