c8ebbc0b03cf5bdee92f734c73e6e654e308bc11
RaulBirlem <raul.birlem@gmail.com>
2023-07-12 20:31:12 -0300

    httpConfig - useFetch

--- my-app-vite-http/src/hooks/useFetch.jsx
+++ my-app-vite-http/src/hooks/useFetch.jsx
@@ -1,22 +1,57 @@
 import { useState, useEffect } from "react";
 
+
+
+
 // custom hook
 
 export const useFetch = (url) => {
     const [data, setData] = useState(null)
 
+    //refactoring post
+    const [config, setConfig] = useState(null)
+    const [method, setMethod] = useState(null)
+    const [callFetch,setCallFetch ] = useState(false)
+
+    const httpConfig = (data,method) {
+        if(method === "POST"){
+            setConfig({
+                method,
+                headers: {
+                    "Content-Type":"application/json"
+                },
+                body: JSON.stringify(data,)
+            })
+        }
+    }
+
     useEffect(()=> {
         const fetchData = async () => {
-            const res = await fetch(url)
+            const res = await fetch(url);
 
-            const json = await res.json()
+            const json = await res.json();
 
-            setData(json)
+            setData(json);
         }
         fetchData();
 
-    }, [url])
+    }, [url]);
 
-    return {data};
+
+    //post refactoring
+    useEffect(()=> {
+        const httpRequest = async () => {
+            if(method === "POST"){
+                let fetchOptions = [url, config];
+                const res = await fetch(...fetchOptions);
+                const json = await res.json();
+                setCallFetch(json)
+            }
+        }
+        httpRequest();
+
+    }, [config]);
+
+    return {data, httpConfig};
 
 }
