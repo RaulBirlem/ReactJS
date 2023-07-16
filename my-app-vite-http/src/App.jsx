dfc1019349505f97ffd4ba18f4fdbd4b60104129
RaulBirlem <raul.birlem@gmail.com>
2023-07-16 16:11:30 -0300

    loading on button

--- my-app-vite-http/src/App.jsx
+++ my-app-vite-http/src/App.jsx
@@ -18,19 +18,7 @@ function App() {
   const [name, setName] = useState("");
   const [price, setPrice] = useState("");
 
-  //resgatar dados
-/*   useEffect(() => {
-    async function fetchData(){
-      const res = await fetch(url);
 
-      const data = await res.json();
-  
-      setProducts(data)
-    }
-
-    fetchData();
-
-  },[]); */
 
   // add products
   const handleSubmit = async(e) =>{
@@ -40,14 +28,7 @@ function App() {
       name,
       price
     }
-/*     const res = await fetch(url, {
-      method: "POST",
-      headers: {
-        "Content-Type":"application/json"
-      },
-      body: JSON.stringify(product),
-    })
-
+/*   
     // dynamic load
     const addedProduct = await res.json();
     setProducts((prevProducts) => [...prevProducts, addedProduct ]) */
@@ -89,10 +70,11 @@ function App() {
                 <input type="number" value={price} 
                 name="price" onChange={(e) => setPrice(e.target.value)} />
               </label>
-              <input type="submit" value="Criar"/>
+              {/* state loading no post */}
+              {loading && <input type="submit" disabled value="Aguarde"/>}
+              {!loading && <input type="submit" value="Criar"/>}
             </form>
         </div>
-
     </>
   )
 }
