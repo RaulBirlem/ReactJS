59473eff0db945d2920f94247f2c07e9b3ddc9b0
RaulBirlem <raul.birlem@gmail.com>
2023-07-16 20:54:58 -0300

    react-router

---
+++ my-app-vite-router/src/App.jsx
@@ -0,0 +1,27 @@
+
+import './App.css'
+
+
+// react router
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+
+//pages
+import Home from './assets/pages/Home'
+import About from './assets/pages/About'
+
+function App() {
+
+  return (
+    <>
+      <h1>React Router</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="about" element={<About/>}/>
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
