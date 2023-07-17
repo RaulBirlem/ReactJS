f72e86e0c8e3b43ac096c3f57b24f05ee01b064d
RaulBirlem <raul.birlem@gmail.com>
2023-07-16 21:02:55 -0300

    link - Navbar

--- my-app-vite-router/src/App.jsx
+++ my-app-vite-router/src/App.jsx
@@ -9,12 +9,16 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Home from './assets/pages/Home'
 import About from './assets/pages/About'
 
+//components
+import Navbar from './components/Navbar'
+
 function App() {
 
   return (
     <>
       <h1>React Router</h1>
       <BrowserRouter>
+      <Navbar/>
         <Routes>
           <Route path="/" element={<Home/>}/>
           <Route path="about" element={<About/>}/>
