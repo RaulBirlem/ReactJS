c71d5465e69415aa1a9323d28eafd08c41ec7925
RaulBirlem <raul.birlem@gmail.com>
2023-07-18 00:07:41 -0300

    navlink - Navbar

--- my-app-vite-router/src/components/Navbar.jsx
+++ my-app-vite-router/src/components/Navbar.jsx
@@ -1,11 +1,13 @@
 import './Navbar.css'
 //links
-import {Link} from 'react-router-dom';
+import {Link, NavLink} from 'react-router-dom';
 
 const Navbar = () => {
   return ( <nav>
-    <Link to="/">Home</Link>
-    <Link to="/about">Sobre</Link>
+{/*     <Link to="/">Home</Link>
+    <Link to="/about">Sobre</Link> */}
+    <NavLink to="/">Home</NavLink>
+    <NavLink to="/about">Sobre</NavLink>
   </nav>
   )
 }
