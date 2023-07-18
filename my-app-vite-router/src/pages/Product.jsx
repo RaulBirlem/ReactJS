b9b6d4a2b005d668d56507bb215ac71125eef0b7
RaulBirlem <raul.birlem@gmail.com>
2023-07-17 23:40:25 -0300

    individual load  dinamic product - Product

--- my-app-vite-router/src/pages/Product.jsx
+++ my-app-vite-router/src/pages/Product.jsx
@@ -1,12 +1,25 @@
 import { useParams } from "react-router-dom"
+import { useFetch } from "../hooks/useFetch"
 
 const Product = () => {
     //dinamic routes
     const {id} = useParams()
 
+    //individual load product
+    const url = "http://localhost:3000/products/" +id
+    const {data:product, loading,error} = useFetch(url)
+    console.log(product)
   return (
    <>
     <p>ID do produto:{id}</p>
+    {error && <p>Falha ao carregar...</p>}
+    {loading && <p>Carregando...</p>}
+    {product && (
+        <div>
+            <h1>{product.name}</h1>
+            <p>R${product.price}</p>
+        </div>
+    )}
    </>
   )
 }
