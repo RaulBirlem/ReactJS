3164afb84362434d6dae8e834e288a2846d6a49c
RaulBirlem <raul.birlem@gmail.com>
2023-07-19 18:42:22 -0300

    provider

--- my-app-vite-context-1/src/context/CounterContext.jsx
+++ my-app-vite-context-1/src/context/CounterContext.jsx
@@ -3,3 +3,14 @@
 import { createContext, useState } from "react";
 
 export default CounterContext = createContext();
+
+// provider
+export const CounterContextProvider = ({children}) => {
+    const [counter, setCounter] = useState()
+
+    return (
+        <CounterContext.Provider value={{counter, setCounter}}>
+            {children}
+        </CounterContext.Provider>
+    )
+}
--- my-app-vite-context-1/src/main.jsx
+++ my-app-vite-context-1/src/main.jsx
@@ -3,8 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import { CounterContextProvider } from './context/CounterContext.jsx'
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {/* provider */}
+    <CounterContextProvider>
+      <App />
+    </CounterContextProvider>
   </React.StrictMode>,
 )
