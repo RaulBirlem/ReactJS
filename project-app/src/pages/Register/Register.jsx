03954347e572842b46052bc35925f1c07e27055c
RaulBirlem <raul.birlem@gmail.com>
2023-07-24 17:48:48 -0300

    register - Register.jsx

--- project-app/src/pages/Register/Register.jsx
+++ project-app/src/pages/Register/Register.jsx
@@ -1,8 +1,32 @@
 import styles from './Register.module.css'
 
+//import { useState, useEffect } from "react-router-dom"
+
 const Register = () => {
   return (
-    <div>Register</div>
+    <>
+      <h1>Cadastre-se para postar</h1>
+      <p>Crie seu usuário e compartilhe suas histórias</p>
+      <form>
+          <label>
+            <span>Nome:</span>
+            <input type="text" name="displayName" required placeholder="Nome de usuário" />
+          </label>
+          <label>
+            <span>E-mail:</span>
+            <input type="email" name="email" required placeholder="E-mail de usuário" />
+          </label>
+          <label>
+            <span>Senha:</span>
+            <input type="password" name="password" required placeholder="Senha de usuário" />
+          </label>
+          <label>
+            <span>Confirmação de senha:</span>
+            <input type="password" name="confirmPassword" required placeholder="Confirme a sua senha" />
+          </label>
+          <button className='btn'>Cadastrar</button>
+      </form>
+    </>
   )
 }
 
