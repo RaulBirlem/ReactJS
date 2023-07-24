985edf401b257e1931245dff29eedec0bb510ac5
RaulBirlem <raul.birlem@gmail.com>
2023-07-24 15:48:05 -0300

    nav links  - Navbar.jsx

--- project-app/src/App.jsx
+++ project-app/src/App.jsx
@@ -6,14 +6,26 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
 
 import './App.css'
 
+
+//pages 
+import Home from "./pages/Home/Home"
+import About from "./pages/About/About"
+import Navbar from "./components/Navbar"
+import Footer from "./components/Footer"
+
 function App() {
 
   return (
     <>
      <BrowserRouter>
+     <Navbar/>
       <div className="container">
-        <Routes></Routes>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/about" element={<About/>}/>
+        </Routes>
       </div>
+      <Footer/>
      </BrowserRouter>
     </>
   )
