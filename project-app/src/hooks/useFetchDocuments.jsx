db39f49ead81b9cdf2332f5e5109a5a699b4b21d
RaulBirlem <raul.birlem@gmail.com>
2023-08-17 14:28:08 -0300

    memory leak

--- project-app/src/hooks/useFetchDocuments.jsx
+++ project-app/src/hooks/useFetchDocuments.jsx
@@ -4,9 +4,72 @@ import { db } from '../firebase/config'
 import {collection,query,orderBy,onSnapshot,where,} from 'firebase/firestore'
 
 
-export const useFetchDocuments = (docCollection, search = null, uid = null) {
+export const useFetchDocuments = (docCollection, search = null, uid = null) =>{
 
     const [documents, setDocuments] = useState(null)
     const [error, setError] = useState(null)
     const [loading, setLoading] = useState(null)
+
+    //deal with memory leak
+
+    const [cancelled, setCancelled] = useState(false)
+
+   
+    useEffect(() => {
+        
+        async function loadData(){
+            if(cancelled) return
+
+            setLoading(true)
+
+            const collectionRef = await collection(db, docCollection)
+
+            try {
+                let q
+                //busca
+                //dashboard
+
+                q = await query(collectionRef, orderBy('createdAt','desc'))
+                
+                //buscar dados mais recentes primeiro
+
+                await onSnapshot(q, (querySnapshot)=> {
+                   //mapear dados, retorna os dados atualizados
+                   //doc do firebase melhor retornar apenas 
+                   //os docs necessarios
+                    setDocuments(
+                        querySnapshot.docs.map((doc)=> ({
+                            //recebe o doc e cria objeto 
+                            // com os dados necessarios
+                            id: doc.id,
+                            ...doc.data(),
+
+                        }))
+                    )
+                })
+
+            setLoading(false)
+
+            } catch (error) {
+                console.log(error)
+                setError(error.message)
+
+                setLoading(false)
+            }
+
+        }
+
+        loadData()
+        // caso algum dado seja alterado
+        //atualiza
+    },[docCollection, search,uid,cancelled])
+
+
+    useEffect(() => {
+        return () => setCancelled(true)
+        //montar dados apenas quando necessário
+    },[])
+
+
+
 }
