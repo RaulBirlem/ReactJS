a7f901117a4cf6bb48b354f711c1fdf4c808c1e7
RaulBirlem <raul.birlem@gmail.com>
2023-12-16 11:21:34 -0300

    move app file

--- crud/src/App.js
+++ crud/src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    //Estado
+    const [usuarios, setUsuarios] = useState([]);
+
+    //Rotas
+    const apiUrl = 'http://localhost:3000/api';
+
+    //Recuperando todos os registros do banco de dados
+    const fetchUsuarios = async () => {
+        const response = await axios.get(`${apiUrl}/usuarios`);
+        setUsuarios(response.data);
+    };
+    //Adicionando um novo registro
+    const addUsuario = async (nome, email) => {
+        const response = await axios.post(`${apiUrl}/usuarios`, {nome, email});
+        if(response.data.success) {
+            fetchUsuarios();
+        }
+    }
+
+   //Atualizando um registro
+   const updateUsuario = async ( id, nome, email) => {
+    const response = await axios.put(`${apiUrl}/usuarios/${id}`, {nome, email});
+    if(response.data.success) {
+        fetchUsuarios();
+    }
+   }
+    
+   //Deletando um registro
+   const deleteUsuario = async (id) => {
+    const response = await axios.delete(`${apiUrl}/usuarios/${id}`);
+    if(response.data.success) {
+        fetchUsuarios();
+    }
+   }
 }
-
-export default App;
