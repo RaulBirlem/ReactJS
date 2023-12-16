dad1ae386515193e3043ff568e9ca0bc1beae3ea
RaulBirlem <raul.birlem@gmail.com>
2023-12-16 11:13:27 -0300

    get usuarios

--- crud/src/crud/App.js
+++ crud/src/crud/App.js
@@ -8,5 +8,11 @@ const App = () => {
     //Rotas
     const apiUrl = 'http://localhost:3000/api';
 
-   
+    //Recuperando todos os registros do banco de dados
+    const fetchUsuarios = async () => {
+        const response = await axios.get(`${apiUrl}/usuarios`);
+        setUsuarios(response.data);
+    };
+
+    
 }
