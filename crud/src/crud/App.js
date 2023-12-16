4cb461db445380afda342bcd39b155466bb85eb7
RaulBirlem <raul.birlem@gmail.com>
2023-12-16 11:11:05 -0300

    First commit

---
+++ crud/src/crud/App.js
@@ -0,0 +1,12 @@
+import React, {useState} from 'react';
+import axios from 'axios';
+
+const App = () => {
+    //Estado
+    const [usuarios, setUsuarios] = useState([]);
+
+    //Rotas
+    const apiUrl = 'http://localhost:3000/api';
+
+   
+}
