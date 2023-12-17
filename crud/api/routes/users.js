74fc1e05f67bb04879571ce9c31bb59bdd50ad86
RaulBirlem <raul.birlem@gmail.com>
2023-12-17 15:45:55 -0300

    routes

--- crud/api/routes/users.js
+++ crud/api/routes/users.js
@@ -1,5 +1,5 @@
 import express from "express";
-import {getUsers} from "../controllers/user.js"
+import {getUsers, addUser, updateUser, deleteUser} from "../controllers/user.js"
 
 
 
@@ -7,5 +7,8 @@ import {getUsers} from "../controllers/user.js"
 const router = express.Router()
 
 router.get("/",getUsers)
+router.post("/",addUser)
+router.put("/:id",updateUser)
+router.delete("/:id",deleteUser)
 
 export default router
