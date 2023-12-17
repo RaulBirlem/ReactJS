7d5be6758b54b307e9c483826df838e79dcfd9db
RaulBirlem <raul.birlem@gmail.com>
2023-12-17 14:34:46 -0300

    crud backend

--- reactgram/backend/app.js
+++ reactgram/backend/app.js
@@ -13,6 +13,14 @@ const app = express();
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 
+//solve cors
+app.use(cors({credentials: true, origin:"http://localhost:3000"}));
+
+//Upload directory
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")))
+
+//DB Connection
+require("./config/db.js");
 
 // routes
 const router = require("./routes/Router.js")
