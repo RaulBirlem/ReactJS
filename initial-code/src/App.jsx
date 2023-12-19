0e47a0e6ec363e5a5b09eace4987c16fcfffab65
RaulBirlem <raul.birlem@gmail.com>
2023-12-19 18:07:35 -0300

    items.map

--- initial-code/src/App.jsx
+++ initial-code/src/App.jsx
@@ -86,12 +86,35 @@ function App() {
         }
     ];
 
+    const shopName = "Jersey Shop Made with React JS"
+
+
     return ( 
         <>
             <section className="items">
-                <h4>Jersey Shop Made with React JS</h4>
+                <h4>{shopName}</h4>
                 
-                <div className="product selected">
+
+                { items.map(item=> 
+                    
+                    <div key={item.id} className="product">
+                        <div className="photo">
+                            <img src={"./img/" + item.photo} />
+                        </div>
+                        <div className="description">
+                            <span className="name">{item.name}</span>
+                            <span className="price">{item.price}</span>
+                            <div className="quantity-area">
+                                <button>-</button>
+                                <span className="quantity">{item.quantity}</span>
+                                <button>+</button>
+                            </div>
+                        </div>
+                    </div>
+                    
+                ) }
+
+               {/*  <div className="product selected">
                     <div className="photo">
                         <img src="././img/real_madrid.webp" />
                     </div>
@@ -217,7 +240,7 @@ function App() {
                             <button>+</button>
                         </div>
                     </div>
-                </div>
+                </div> */}
             </section>
             
 
