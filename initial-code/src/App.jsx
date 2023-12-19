60f0aeec2465bcb524a08fe616eec1d4ca626887
RaulBirlem <raul.birlem@gmail.com>
2023-12-19 18:33:18 -0300

    isInBag selected

--- initial-code/src/App.jsx
+++ initial-code/src/App.jsx
@@ -97,18 +97,20 @@ function App() {
 
                 { items.map(item=> 
                     
-                    <div key={item.id} className="product">
+                    <div key={item.id} className={`product ${item.isInBag ? 'selected' : ''}`}>
                         <div className="photo">
                             <img src={"./img/" + item.photo} />
                         </div>
                         <div className="description">
                             <span className="name">{item.name}</span>
                             <span className="price">{item.price}</span>
+                        {   item.isInBag && /* se isInBag for false ele não exibe */
                             <div className="quantity-area">
                                 <button>-</button>
                                 <span className="quantity">{item.quantity}</span>
                                 <button>+</button>
                             </div>
+                        }
                         </div>
                     </div>
                     
