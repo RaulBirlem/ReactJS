61a83eb4ae27f613998fb9e21abc796ac46154c5
RaulBirlem <raul.birlem@gmail.com>
2023-12-20 04:04:50 -0300

    initialTodos,deleteHandler, toggleIsDoneHandler

--- todo-list-context-reducers/src/components/TodosList.jsx
+++ todo-list-context-reducers/src/components/TodosList.jsx
@@ -1,22 +1,61 @@
+import { useState } from 'react';
 import Todo from './Todo.jsx';
 
+const initialTodos = [
+  { id: 0, title: 'Do Groceries', description: 'Buy apples, rice, juice and toilet paper.', isDone: true },
+  { id: 1, title: 'Study React', description: 'Understand context & reducers.', isDone: false},
+  { id: 2, title: 'Learn Redux', description: 'Learn state management with Redux', isDone: false }
+];
+
+
+
+
+
 function TodosList() {
+
+  const [todos, setTodos] = useState(initialTodos);
+
+
+  function deleteHandler(id){
+    if(confirm('Are you sure you want delete the to-do?')){
+      setTodos(todos.filter(todo => todo.id !== id));
+    }
+  }
+
+
+  function toggleIsDoneHandler(id){
+      setTodos(todos.map(todo => {
+        if (todo.id === id){
+          todo.isDone = !todo.isDone;
+          return todo;
+        } else {
+          return todo;
+        }
+      }));
+
+    }
+  
+
+
+
+
+
+
+
 
   return (
     <>
         <div className="todos">
 
-            <Todo
-              title="Do Groceries"
-              description="Buy apples, rice, juice and toilet paper."
-              isDone={true}
+            {todos.map(todo => 
+              <Todo
+              deleteTodo={(id)=> deleteHandler(id)}
+              toggleIsDone={(id)=> toggleIsDoneHandler(id)}
+              todo={todo}/* prop */
+              key={todo.id}
             />
-
-            <Todo
-              title="Study React"
-              description="Understand context, reducers and state management with Redux."  
-              isDone={false}
-            />
+            )}
+            
 
         </div>
     </>
