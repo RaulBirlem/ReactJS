6b3a2a6b52c42715f38109350f5aef16a1a3173e
RaulBirlem <raul.birlem@gmail.com>
2023-12-20 04:13:05 -0300

    context

--- todo-list-context-reducers/src/App.jsx
+++ todo-list-context-reducers/src/App.jsx
@@ -1,16 +1,44 @@
 import Header from './components/Header.jsx';
 import Home from './views/Home.jsx';
+import { TodosContext } from './TodosContext.js';
+
+
 import './App.scss';
+import { useState } from 'react';
+
+
+
+const initialTodos = [
+  { id: 0, title: 'Do Groceries', description: 'Buy apples, rice, juice and toilet paper.', isDone: true },
+  { id: 1, title: 'Study React', description: 'Understand context & reducers.', isDone: false},
+  { id: 2, title: 'Learn Redux', description: 'Learn state management with Redux', isDone: false }
+];
+
+
+
+
+
+
+
+
 
 function App() {
+
+const [todos, setTodos] = useState(initialTodos);
+
+
+
+
 
   return (
     <>
       <main>
-        
-        <Header appName="To-Do List with React" />
+        <TodosContext.Provider value={{todos, setTodos}}>
+          <Header appName="To-Do List with React" />
 
-        <Home />
+           <Home />
+        </TodosContext.Provider>
+
 
       </main>
     </>
