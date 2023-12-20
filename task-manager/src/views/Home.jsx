a786dd3e781c45ac1cdc6ac872a35dd692d85ef5
RaulBirlem <raul.birlem@gmail.com>
2023-12-20 03:30:56 -0300

    module scss

--- task-manager/src/views/Home.jsx
+++ task-manager/src/views/Home.jsx
@@ -1,5 +1,6 @@
 import Filter from '../components/Filter.jsx';
 import TodosList from '../components/TodosList.jsx';
+import FooterFilters from '../components/FooterFilters.jsx';
 
 function Home() {
 
@@ -10,6 +11,11 @@ function Home() {
         
             <TodosList />
         </div>
+
+        <FooterFilters/>
+
+
+
     </>
     )
   }
